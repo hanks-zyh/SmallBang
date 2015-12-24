02804d2a4cdff590f9f40ddb04c376dd0e1a893a
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-24 21:03:46 +0800

    add SmallBangListener

---
+++ library/src/main/java/xyz/hanks/library/SmallBangListener.java
@@ -0,0 +1,17 @@
+package xyz.hanks.library;
+
+/**
+ * for animation callback
+ * Created by hanks on 15/12/24.
+ */
+public interface SmallBangListener {
+    /**
+     * <p>Notifies the start of the animation.</p>
+     */
+    void onAnimationStart();
+
+    /**
+     * <p>Notifies the end of the animation. </p>
+     */
+    void onAnimationEnd();
+}
