51549169082baa9d975c0830a233db159df633c3
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-24 20:55:46 +0800

    add hear.png add view animator update Animation_time

--- library/src/main/java/xyz/hanks/library/SmallBang.java
+++ library/src/main/java/xyz/hanks/library/SmallBang.java
@@ -2,16 +2,19 @@ package xyz.hanks.library;
 
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.OvershootInterpolator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +26,8 @@ import java.util.Random;
  */
 public class SmallBang extends View {
 
-    int[] colors = {0xFFDF4288, 0xFFCD8BF8,0XFF2B9DF2,0XFFA4EEB4,0XFFE097CA,0XFFCAACC6,0XFFC5A5FC,0XFFF5BC16,0XFFF2DFC8,0XFFE1BE8E,0XFFC8C79D};
+    int[] colors = {0xFFDF4288, 0xFFCD8BF8, 0XFF2B9DF2, 0XFFA4EEB4, 0XFFE097CA, 0XFFCAACC6, 0XFFC5A5FC, 0XFFF5BC16, 0XFFF2DFC8, 0XFFE1BE8E, 0XFFC8C79D};
+    List<Dot> dotList = new ArrayList<>();
     private long ANIMATE_DURATION = 1000;
     private float MAX_RADIUS = 150;
     private float MAX_CIRCLE_RADIUS = 100;
@@ -36,8 +40,7 @@ public class SmallBang extends View {
     private int DOT_NUMBER = 16;
     private float DOT_BIG_RADIUS = 8;
     private float DOT_SMALL_RADIUS = 5;
-
-
+    private int[] mExpandInset = new int[2];
     // 将下面的view变小
     // 画圆半径从小到大,同时颜色渐变 (P1)
     // 当半径到达 MAX_RADIUS, 开始画空心圆,空闲圆半径变大,画笔宽度从MAX_RADIUS变小
@@ -73,6 +76,38 @@ public class SmallBang extends View {
 
     }
 
+    public void bang(final View view) {
+        Rect r = new Rect();
+        view.getGlobalVisibleRect(r);
+        int[] location = new int[2];
+        getLocationOnScreen(location);
+        r.offset(-location[0], -location[1]);
+        r.inset(-mExpandInset[0], -mExpandInset[1]);
+        int startDelay = 100;
+        view.setScaleX(0.1f);
+        view.setScaleY(0.1f);
+        ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f).setDuration((long) (ANIMATE_DURATION*0.5f));
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+//            Random random = new Random();
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+//                view.setTranslationX((random.nextFloat() - 0.5f) * view.getWidth() * 0.05f);
+//                view.setTranslationY((random.nextFloat() - 0.5f) * view.getHeight() * 0.05f);
+                float animatedFraction = animation.getAnimatedFraction();
+                view.setScaleX(0.1f + animatedFraction*0.9f);
+                view.setScaleY(0.1f + animatedFraction*0.9f);
+            }
+        });
+        animator.setInterpolator(new OvershootInterpolator(2));
+        animator.setStartDelay((long) (ANIMATE_DURATION * P3));
+        animator.start();
+//        view.animate().setDuration(150).setStartDelay(startDelay).scaleX(0f).scaleY(0f).alpha(0f).start();
+        //explode(Utils.createBitmapFromView(view), r, startDelay, ExplosionAnimator.DEFAULT_DURATION);
+        bang();
+    }
+
     public void bang() {
         ValueAnimator valueAnimator = new ValueAnimator().ofFloat(0, 1).setDuration(ANIMATE_DURATION);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -86,11 +121,10 @@ public class SmallBang extends View {
         initDots();
     }
 
-    List<Dot> dotList = new ArrayList<>();
-    private void initDots(){
+    private void initDots() {
 
         Random random = new Random(System.currentTimeMillis());
-        for (int i = 0; i < DOT_NUMBER*2; i++) {
+        for (int i = 0; i < DOT_NUMBER * 2; i++) {
             Dot dot = new Dot();
             dot.startColor = colors[random.nextInt(99999) % colors.length];
             dot.endColor = colors[random.nextInt(99999) % colors.length];
@@ -104,7 +138,7 @@ public class SmallBang extends View {
 
         Log.d("SmallBang", "draw" + progress);
 
-        if(progress>=0 && progress<=P1) {
+        if (progress >= 0 && progress <= P1) {
             float progress1 = 1f / P1 * progress;
             if (progress1 > 1) progress1 = 1;
             int startColor = colors[0];
@@ -112,9 +146,9 @@ public class SmallBang extends View {
             circlePaint.setStyle(Paint.Style.FILL);
             circlePaint.setColor(evaluateColor(startColor, endColor, progress1));
             canvas.drawCircle(getWidth() / 2, getHeight() / 2, MAX_CIRCLE_RADIUS * progress1, circlePaint);
-        }else if(progress>P1){
+        } else if (progress > P1) {
 
-            if(progress>P1 && progress<=P3) {
+            if (progress > P1 && progress <= P3) {
                 float progress2 = (progress - P1) / (P3 - P1);
                 if (progress2 < 0) progress2 = 0;
                 if (progress2 > 1) progress2 = 1;
@@ -150,13 +184,18 @@ public class SmallBang extends View {
                 }
             }
         }
-    }
 
 
-    class Dot {
-        int startColor;
-        int endColor;
     }
+
+    public static SmallBang attach2Window(Activity activity) {
+        ViewGroup rootView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
+        SmallBang smallBang = new SmallBang(activity);
+        rootView.addView(smallBang, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        return smallBang;
+    }
+
 
     private int evaluateColor(int startValue, int endValue, float fraction) {
         if (fraction <= 0) {
@@ -178,5 +217,10 @@ public class SmallBang extends View {
         int endB = endInt & 0xff;
 
         return ((startA + (int) (fraction * (endA - startA))) << 24) | ((startR + (int) (fraction * (endR - startR))) << 16) | ((startG + (int) (fraction * (endG - startG))) << 8) | ((startB + (int) (fraction * (endB - startB))));
+    }
+
+    class Dot {
+        int startColor;
+        int endColor;
     }
 }
--- simple/src/main/java/xyz/hanks/smallbang/MainActivity.java
+++ simple/src/main/java/xyz/hanks/smallbang/MainActivity.java
@@ -15,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         smallBang = (SmallBang)findViewById(R.id.smallbang);
+
     }
 
     public void show(View view){
-        smallBang.bang();
+        smallBang.bang(findViewById(R.id.image));
     }
 }
